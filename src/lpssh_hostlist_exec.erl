8bd84bb8c86b4de7caa621ea0b323be75b1e149f
Stanislaw Klekot <dozzie@jarowit.net>
2014-02-25 19:46:17 +0100

    Added actually running a command for lpssh_hostlist_exec.
    The query can take one of two forms: a regexp or an argument appended to
    command.

--- src/lpssh_hostlist_exec.erl
+++ src/lpssh_hostlist_exec.erl
@@ -1,6 +1,8 @@
 %%%---------------------------------------------------------------------------
 %%% @doc
 %%%   Host inventory based on OS's `exec*()' function.
+%%%
+%%% @TODO Change {@link os:cmd/1} to ports.
 %%% @end
 %%%---------------------------------------------------------------------------
 
@@ -16,6 +18,10 @@
 -export([]).
 
 %%%---------------------------------------------------------------------------
+
+-record(opts, {re_cmd = undefined, arg_cmd = undefined}).
+
+%%%---------------------------------------------------------------------------
 %%% gen_lpssh_hostlist API
 %%%---------------------------------------------------------------------------
 
@@ -29,19 +35,25 @@ dependencies() ->
   [].
 
 %% @doc Setup environment.
+%%   In this case `Args' is of form `{re, Command :: string()}' (command is
+%%   run as is and the `Query' is regexp) or `{arg, Command :: string()}'
+%%   (`Query' is appended to the command before running). See {@link hosts/2}
+%%   for details.
 %%
 %% @spec init(term()) ->
 %%   {ok, State} | {error, Reason}
 
-init(_Args) ->
-  {ok, nostate}.
+init({re, Command} = _Args) ->
+  {ok, #opts{re_cmd = Command}};
+init({arg, Command} = _Args) ->
+  {ok, #opts{arg_cmd = Command}}.
 
 %% @doc Clean up on shutdown.
 %%
 %% @spec terminate(term()) ->
 %%   any()
 
-terminate(nostate = _State) ->
+terminate(_State) ->
   ok.
 
 %% @doc List all known hosts to be called.
@@ -49,18 +61,32 @@ terminate(nostate = _State) ->
 %% @spec hosts(term()) ->
 %%   {ok, [string()]}
 
-hosts(nostate = _State) ->
-  % TODO: implement me
-  {ok, ["localhost"]}.
+hosts(_State = #opts{arg_cmd = Command, re_cmd = undefined}) ->
+  Output = os:cmd(Command),
+  Hosts = string:tokens(Output, "\r\n"),
+  {ok, Hosts};
+hosts(_State = #opts{re_cmd = Command, arg_cmd = undefined}) ->
+  Output = os:cmd(Command),
+  Hosts = string:tokens(Output, "\r\n"),
+  {ok, Hosts}.
 
 %% @doc List hosts matching to the query to be called.
+%%   Depending on {@link init/1} option, the query is either string to be
+%%   appended to the command or a regexp to filter the results.
 %%
 %% @spec hosts(string(), term()) ->
 %%   {ok, [string()]}
 
-hosts(_Query, nostate = _State) ->
-  % TODO: implement me
-  {ok, ["localhost"]}.
+hosts(Query, _State = #opts{arg_cmd = Command, re_cmd = undefined}) ->
+  Output = os:cmd(Command ++ " " ++ Query),
+  Hosts = string:tokens(Output, "\r\n"),
+  {ok, Hosts};
+hosts(Query, _State = #opts{re_cmd = Command, arg_cmd = undefined}) ->
+  % TODO: handle pattern errors
+  {ok, Re} = re:compile(Query),
+  Output = os:cmd(Command),
+  Hosts = string:tokens(Output, "\r\n"),
+  {ok, [H || H <- Hosts, re:run(H, Re, [{capture, none}]) == match]}.
 
 %% @doc Return description of an error returned by {@link hosts/1}.
 %%
